8dcc347beafc058bbb715eef02ac2cdd1bcc7ee6
Montana-Lee-DoubleMe <montana.lee@doubleme.me>
2022-11-30 13:55:49 +0900

    markerScene

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/marker_abyss.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/web_ar_test.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
