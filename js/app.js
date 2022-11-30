4860ddb0db1ba9d7f6d8e163658a3055e90299b2
Montana-Lee-DoubleMe <montana.lee@doubleme.me>
2022-11-30 14:09:42 +0900

    arTest

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/web_ar_test.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/4.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
